4b97d668dad40cd9c2fd30bcb7d46c32f94c4098
SRIKANTH SAMPATHI <srikanthsampathi82@gamail.com>
2024-03-16 11:38:53 +0530

    game-backend

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const User = require("./models/user");
+require("dotenv").config();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const port = process.env.PORT;
+
+mongoose
+  .connect(process.env.DB)
+  .then(() => {
+    console.log("Connected!");
+    app.listen(port, "0.0.0.0", () => {
+      console.log(`server is running ${port}`);
+    });
+  })
+  .catch((e) => {
+    console.log("connection failed", e);
+  });
+
+app.get("/", (req, res) => {
+  res.send("<h1>HELLO THIS GAME API</h1>");
+});
+
+app.post("/register", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const user = new User({ username });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Failed to register user" });
+  }
+});
+
+app.post("/record-game", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const user = await User.findOneAndUpdate(
+      { username },
+      { $inc: { points: 1 } }
+    );
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.status(200).json({ message: "Game recorded successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Failed to record game" });
+  }
+});
+
+app.get("/leaderboard", async (req, res) => {
+  try {
+    const leaderboard = await User.find()
+      .sort({ score: -1 })
+      .select("username points");
+    res.status(200).json(leaderboard);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to retrieve leaderboard" });
+  }
+});
---
+++ models/user.js
@@ -0,0 +1,22 @@
+// models/user.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  points: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
